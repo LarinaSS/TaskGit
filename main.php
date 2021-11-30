d974321116f96821e7ba0ad45ab574ddf5d43177
Sveta <larina_SS@mail.ru>
2021-11-30 10:25:34 +0300

    Creation form table

--- main.php
+++ main.php
@@ -15,3 +15,15 @@
         <input type="submit" name="subBack" class ="subData" value="Вернуться"> 
     </form>
 </div>
+<div id="contentForm">
+    <div id="myForm">
+        <form action ="" method="post" class="formContainer">
+            <h2 id="heading">Добавление задачи</h2>
+            <label>Название</label><br>
+            <input type="text" name="name" value="" required><br>
+            <label>Дата</label><br>
+            <input type="text" name="data" value="" required><br>
+            <button type="submit" class="btn" onclick="closeForm()">Добавить</button>
+        </form>
+    </div>
+</div>
