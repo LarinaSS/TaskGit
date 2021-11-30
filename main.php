f9f95ff92c4389df8a59c1d0a2187c883a982cf4
Sveta <larina_SS@mail.ru>
2021-11-30 10:22:48 +0300

    Creation header

---
+++ main.php
@@ -0,0 +1,8 @@
+<div id="promo">
+    <h1 id="promoText">
+        Планировщик задач 
+    </h1>
+</div>
+<div id="add">
+    <button id="open-button" onclick="openForm()">Добавить задачу</button> 
+</div>
