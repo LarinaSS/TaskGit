84d7140d030cd761853a97a8609a2ee982f1d52d
Sveta <larina_SS@mail.ru>
2021-11-30 10:24:20 +0300

    Creation form search

--- main.php
+++ main.php
@@ -6,3 +6,12 @@
 <div id="add">
     <button id="open-button" onclick="openForm()">Добавить задачу</button> 
 </div>
+<div id="searchForm">
+    <form action ="" method="post" class="formSearch">
+        <input type="text" name="dataSearch" class="dataSearch" placeholder="Поиск по названию">
+        <input type="submit" name="subData" class ="subData" value="Поиск"> 
+        <input type="text" name="search" class="search" placeholder="Поиск по дате">
+        <input type="submit" name="sub" class ="subData" value="Поиск"> 
+        <input type="submit" name="subBack" class ="subData" value="Вернуться"> 
+    </form>
+</div>
