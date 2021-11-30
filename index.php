a69305fbe9da585e2a3cb3ec1b5a43778dd4d4cb
Sveta <larina_SS@mail.ru>
2021-11-30 08:58:52 +0300

    Add file index.php and sit.css

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link href="styles/sit.css" rel="stylesheet">
+    <script src="scripts/form.js"></script>
+    <title>Планировщик задач</title>
+</head>
+<body>
+<div id="content">
+    <?php
+    $page = $_GET['page'];
+    if (!isset($page)) {
+        require('templates/main.php');
+    }  
+?>
+<div>
+</body>
+</html>
